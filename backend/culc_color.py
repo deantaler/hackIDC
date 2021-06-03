61b263b663ecfe60df22fe1d08ec3688524b5bb5
Dean Taler <deantaler@gmail.com>
2021-06-04 01:47:58 +0300

    added requirements.txt

--- backend/culc_color.py
+++ backend/culc_color.py
@@ -11,7 +11,8 @@ def get_n_closest(rgb: (int, int, int), n: int) -> pd.DataFrame:
     fan_colors_df = pd.read_csv('fan_colors.csv')
     fan_colors_df['RGB_tuple'] = fan_colors_df['RGB'].apply(literal_eval)
     fan_colors_df['distance'] = fan_colors_df['RGB_tuple'].apply(get_distance, close_rgb=rgb)
-    return fan_colors_df.nsmallest(n, 'distance').reset_index()
+    df = fan_colors_df.nsmallest(n, 'distance').reset_index()
+
 
 
 # print(get_n_closest(rgb=(245, 236, 217), n=3))
