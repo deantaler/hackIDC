76743e239762df740c21eacaae838101baaeff80
Dean Taler <deantaler@gmail.com>
2021-06-04 12:24:49 +0300

    works

--- src/components/ImageUploadForm/ImageGallery/ImageGallery.js
+++ src/components/ImageUploadForm/ImageGallery/ImageGallery.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import imageGalleryStyles from "./ImageGallery.module.scss"
 
 const ImageGallery = (props) =>
-  props.images.map((image, i) => (
-    <div key={i} className="fadein">
-      <div
-        onClick={() => props.removeImage(image.public_id)}
-        className="delete"
-      >
-        <FontAwesomeIcon icon={faTimesCircle} size="2x" />
-      </div>
-      <img src={image.secure_url} alt="" />
+<div className={imageGalleryStyles.container}>
+
+  {props.colors.map((color, i) => (
+    <div key={i} className={imageGalleryStyles.colorBlock} style={{backgroundColor: `rgb(${color.RGB})`}}>
+      
     </div>
-  ));
+  ))}
+</div>
 
 export default ImageGallery;
--- src/components/ImageUploadForm/UploadButton/UploadButton.js
+++ src/components/ImageUploadForm/UploadButton/UploadButton.js
@@ -1,14 +1,38 @@
 import React from "react";
 
-const UploadButton = (props) => <div className="file-field input-field">
-    <div className="btn">
+const UploadButton = ({boardRef, colorRef, onSubmitHandler}) => {
+
+const buttonStyles = {minWidth: "200px"}
+return <div id="form">
+
+<div className="file-field input-field">
+  <div id="to_hide">
+    <div className="btn" style={buttonStyles}>
       <span>Board Photo</span>
-      <input type="file" name="board" id="single" onChange={props.onChange} />
+      <input type="file" name="board" id="single" ref={boardRef} />
+    </div>
+    <div className="file-path-wrapper">
+      <input className="file-path validate" type="text" placeholder="Upload one or more files"/>
+    </div>
+  </div>
+  <div className="file-field input-field">
+    <div className="btn" style={buttonStyles}>
+      <span>Requested Color</span>
+      <input type="file" name="color" id="single" ref={colorRef}/>
     </div>
     <div className="file-path-wrapper">
       <input className="file-path validate" type="text" />
     </div>
   </div>
-  
+  <a className="waves-effect waves-light btn" style={{...buttonStyles, display:"block", margin:"0 auto", width:"50%"}} onClick={onSubmitHandler}>Submit</a>
+  </div>
+  <div id = "to_show" style={{"display":"hidden"}}>
+    <div id ="color">
+    </div>
+  </div>
+</div>
+
+
+}
 
 export default UploadButton;
